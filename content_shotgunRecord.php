244f2f012ab45ba73e1f51684af25ce47770e6d1
Antoine <Antoine@192.168.1.22>
2016-12-24 17:49:17 +0100

    RAS

---
+++ content_shotgunRecord.php
@@ -0,0 +1,11 @@
+<?php
+
+// Ici on est chargé de vérifier que l'argument shotgunId qu'on nous fournit dans le get est bien exploitable.
+
+/* Si je suis user simple, je ne peux voir que les shotguns dont la date de publi est dépassée, qui sont ouverts ET actifs.
+ * 
+ * Si je suis admin, je peux tout voir, mais j'ai assez d'infos pour m'y retrouver quand même.
+ */
+
+
+?>
